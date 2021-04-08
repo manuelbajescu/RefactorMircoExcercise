9ef3cd92bcd916acd8c34b6147c465b22bc63efc
manuelbajescu <bajescu_manuel@yahoo.com>
2021-04-08 18:52:20 +0300

    i made unitary test for 1 ad 2 exercise

---
+++ TDDMicroExercisesTests/UnitTest1.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using System.Reflection;
+using TDDMicroExercises.TirePressureMonitoringSystem;
+using TDDMicroExercises.UnicodeFileToHtmlTextConverter;
+
+namespace TDDMicroExercisesTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+
+        public void TestAlarm()
+        {
+            Alarm alarm = new Alarm();
+            alarm.Check();
+            if (alarm.GetPsiPreasure > 17 && alarm.GetPsiPreasure < 21)
+            {
+                Assert.AreEqual(alarm.AlarmOn, false);
+            }
+            else
+            {
+                Assert.AreEqual(alarm.AlarmOn, true);
+            }
+        }
+
+        [TestMethod]
+        public void TestUnicodeFileToHtmlTextConverter()
+        {
+            string expectedResult = "test<br />";
+            string testFilePath = "C:\\Temp\\UnicodeFileToHtmlConverterTestFile.txt";
+            var htmlConvertor = new UnicodeFileToHtmlTextConverter(testFilePath);
+            string result = htmlConvertor.ConvertToHtml();
+
+            Assert.AreEqual(result, expectedResult);
+        }
+    }
+}
